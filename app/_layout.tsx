d4c1586f5cbf3c3839be3368e5c97e2f24724568
Nam Dang <nathandang73@gmail.com>
2025-03-22 22:58:11 +1100

    Implement routing and navigation

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,27 @@
 import { Stack } from "expo-router";
 import "./globals.css";
+import { StatusBar } from "react-native";
+import React from "react";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <>
+      <StatusBar hidden={true} />
+
+      <Stack>
+        <Stack.Screen
+          name="(tabs)"
+          options={{
+            headerShown: false,
+          }}
+        />
+        <Stack.Screen
+          name="movie/[id]"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+    </>
+  );
 }
