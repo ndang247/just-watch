aebc629fcd69cf469a31454626432995fa4237f5
Nam Dang <nathandang73@gmail.com>
2025-03-25 00:10:10 +1100

    Implement home screen to display popular movies

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,76 @@
+import MovieCard from "@/components/MovieCard";
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
-import { View, Image, ScrollView } from "react-native";
+import React from "react";
+import {
+  Text,
+  View,
+  Image,
+  ScrollView,
+  ActivityIndicator,
+  FlatList,
+} from "react-native";
 
 export default function Index() {
   const router = useRouter();
+
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() => fetchMovies({ query: "" }));
+
+  const renderContent = () => {
+    if (moviesLoading) {
+      return (
+        <ActivityIndicator
+          size="large"
+          color="#0000ff"
+          className="mt-10 self-center"
+        />
+      );
+    }
+
+    if (moviesError) {
+      return <Text>Error: {moviesError?.message}</Text>;
+    }
+
+    return (
+      <View className="flex-1 mt-5">
+        <SearchBar
+          onPress={() => {
+            router.push("/search");
+          }}
+          placeholder="Search for a movie"
+        />
+
+        <>
+          <Text className="text-lg text-white font-bold mt-5 mb-3">
+            Latest Movies
+          </Text>
+
+          <FlatList
+            data={movies}
+            renderItem={({ item }) => <MovieCard {...item} />}
+            keyExtractor={(item) => item.id.toString()}
+            numColumns={3}
+            columnWrapperStyle={{
+              justifyContent: "flex-start",
+              gap: 20,
+              paddingRight: 5,
+              marginBottom: 10,
+            }}
+            className="mt-2 pb-32"
+            scrollEnabled={false}
+          />
+        </>
+      </View>
+    );
+  };
 
   return (
     <View className="flex-1 bg-primary">
@@ -16,15 +81,7 @@ export default function Index() {
         contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
       >
         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
-
-        <View className="flex-1 mt-5">
-          <SearchBar
-            onPress={() => {
-              router.push("/search");
-            }}
-            placeholder="Search for a movie"
-          />
-        </View>
+        {renderContent()}
       </ScrollView>
     </View>
   );
